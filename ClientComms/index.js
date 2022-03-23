6350f8dfd482aa0e48d67748e7bf5a4f9e0111e4
The_Raven <mikkel@meldgaard.net>
2022-03-23 11:34:57 +0100

    Added socketio connection

--- ClientComms/index.js
+++ ClientComms/index.js
@@ -1,19 +1,11 @@
-const express = require("express");
-const app = express();
+const { Server } = require("socket.io");
 
-const port = process.env.PORT || 3001;
+const io = new Server({cors: {
+    origin: "*",
+}});
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    }
-);
-
-app.get("/data/", (req, res) => {
-    res.send("Such data! much wow!");
-    }
-);
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+io.on("connection", (socket) => {
+    console.log("a user connected");
 });
+
+io.listen(3001);
--- ReactServer/saenkeslagskib/src/App.js
+++ ReactServer/saenkeslagskib/src/App.js
@@ -1,7 +1,12 @@
 import Board from "./board";
-
+import io from "socket.io-client";
  
 function App() {
+  const socket = io("http://localhost:3001");
+  socket.on("connect", () => {
+    console.log(socket.id);
+  });
+
   return (
     <div className="App">
       <h1>Hello There</h1> 
