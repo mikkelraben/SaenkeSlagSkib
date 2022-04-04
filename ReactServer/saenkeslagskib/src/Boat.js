83273e67f5bc4fa27bdc1b11771b4f828b4b92da
Mikael Lehrmann Kashlev <mika3033@edu.aarhustech.dk>
2022-04-04 13:07:25 +0200

    Removed the  variable that defined amount if ships

--- ReactServer/saenkeslagskib/src/Boat.js
+++ ReactServer/saenkeslagskib/src/Boat.js
@@ -1,33 +1,29 @@
 //Defenition of what a boat is, andt it's parameters.
 
-class Boat {
-    constructor(x,y,length,orientation){
+class Boat {  
+    constructor(x,y,length,orientation){ //Constructor for the boat.
         this.x = x;
         this.y = y;
         this.length = length;
-        this.orientation = orientation;
+        this.orientation = orientation;  
     }
 
 
-    SetShipLength() {
-        const minBoat = 3;
-        const maxBoat = parseInt(this.size / 2) +1;
-        const numberOfBoats = this.GetRandomNumber(minBoat, maxBoat);
-
+    SetShipLength() { //Sets the length of the boat.
         const minBoatLength = 2;
-        const maxBoatLength = parseInt(this.size / 2);
+        const maxBoatLength = parseInt(this.size / 2); //The max length of the boat is half the size of the board.
 
-        for (let i = 0; i < numberOfBoats; i++) {
-            const boatLength = this.GetRandomNumber(minBoatLength, maxBoatLength);
+        for (let i = 0; i < this.size; i++) {
+            const boatLength = this.GetRandomNumber(minBoatLength, maxBoatLength); //Gets a random number between the min and max boat length.
             const orientation = this.GetRandomNumber(0, 1);
-            const x = this.GetRandomNumber(0, this.size - 1);
-            const y = this.GetRandomNumber(0, this.size - 1);
+            const x = this.GetRandomNumber(0, this.size - 1); //Gets a random x coordinate.
+            const y = this.GetRandomNumber(0, this.size - 1); //Gets a random y coordinate.
 
-            this.boats.push(new Boat(x, y, boatLength, orientation));
+            this.boats.push(new Boat(x, y, boatLength, orientation)); //Adds the boat to the array.
         }
     }
 
-    SetAllBoats() {
+    SetAllBoats() { //Sets all the boats on the board.
         for (let i = 0; i < this.boats.length; i++) {
             this.SetBoat(this.boats[i]);
         }
@@ -36,8 +32,8 @@ class Boat {
     }
 
 
-    BuildBoats() {
-        let boardCopy = Boat.CopyArrayOfObjects(this.board);
+    BuildBoats() { //Builds the boats on the board.
+        let boardCopy = Boat.CopyArrayOfObjects(this.board); 
         let direction = this.GetDirections();
         let BoatCoordinates = [];
         let startPoint = {
@@ -47,39 +43,43 @@ class Boat {
 
         let curentPoint;
 
-        for (let i = 0; i < this.boats.length; i++) {
+        for (let i = 0; i < this.boats.length; i++) { //Loops through all the boats.
             
-            let nextPoint =
-               
-                direction[this.boats[i].orientation]
-                    (startPoint.x, startPoint.y, this.boats[i].length);
+            let nextPoint = {
+                x: startPoint.x,
+                y: startPoint.y
+            };
 
-            if (this.IsValidPoint(nextPoint)) {
+            if (this.IsValidPoint(nextPoint)) { //Checks if the next point is valid.
                 curentPoint = nextPoint;
                 startPoint = nextPoint;
                 BoatCoordinates.push(curentPoint);
-            } else {
+            } else { //If the next point is not valid, it will try to find a new point.
                 i--;
             }
 
-            if(!this.IsValidPoint(boardCopy,nextPoint)){
+            if(!this.IsValidPoint(boardCopy,nextPoint)){ //If the boat is not valid, it will be removed from the array.
                 this.invalidAttempts += 1;
                 this.BuildBoats(Boat);
                 return;
             }
             curentPoint = nextPoint;
             BoatCoordinates.push(curentPoint);
-            this.SetBoat(this.boats[i], boardCopy, curentPoint);
+            this.SetBoat(this.boats[i], boardCopy, curentPoint);  
 
-            if (this.boats[i].orientation === 0) {
+            if (this.boats[i].orientation === 0) { //If the boat is horizontal.
                 direction = this.GetDirections();
             }
 
-            if (this.boats[i].orientation === 1) {
-                direction = this.GetDirections();
+            nextPoint = this.GetNextPoint(curentPoint, direction); //Gets the next point.
+
+            if (this.IsValidPoint(nextPoint)) { 
+                curentPoint = nextPoint;
+                startPoint = nextPoint;
+                BoatCoordinates.push(curentPoint); 
+            } else {
+                i--;
             }
-
-
         }
 
     }
@@ -87,4 +87,6 @@ class Boat {
     
 
        
-} export default Boat;
+} export default Boat; //Exports the class.
+
+
