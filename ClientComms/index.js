caa185b42f15f424398e1440f12f45368f38b01a
Mikkel Johan Meldgaard Raben <mikkel@meldgaard.net>
2022-05-10 20:41:42 +0200

    Added player logic

--- ClientComms/index.js
+++ ClientComms/index.js
@@ -4,6 +4,49 @@ const io = new Server({cors: {
     origin: "*",
 }});
 
+var player1 = null;
+var player2 = null;
+var player1Boats = [];
+var player2Boats = [];
+var player1BoatsPlaced = false;
+var player2BoatsPlaced = false;
+var player1Turn = true;
+
+const checkifReady = () => {
+    console.log(player1BoatsPlaced, player2BoatsPlaced);
+    //console.log(player1, player2);
+    if(player1 && player2 && player1BoatsPlaced && player2BoatsPlaced){
+        io.emit("StartGame", {player1Boats, player2Boats});
+    }
+}
+
+const addPlayer = (player, boats) => {
+    if(player1 === null){
+        player1 = player;
+        player1Boats = boats;
+        player1BoatsPlaced = true;
+        checkifReady();
+    } else if(player2 === null){
+        player2 = player;
+        player2Boats = boats;
+        player2BoatsPlaced = true;
+        checkifReady();
+    }
+}
+
+const removePlayer = (player) => {
+    if(player === player1){
+        player1 = null;
+        player1Boats = [];
+        player1BoatsPlaced = false;
+    } else if(player === player2){
+        player2 = null;
+        player2Boats = [];
+        player2BoatsPlaced = false;
+    }
+    checkifReady();
+}
+
 io.on("connection", (socket) => {
     socket.on("Init", data => {
         //parse data from json to list of boats
@@ -15,6 +58,8 @@ io.on("connection", (socket) => {
             }
             console.log(boats);
             socket.broadcast.emit("Init", boats);
+            addPlayer(socket, boats);
+            checkifReady();
         } catch (error) {
             console.err(error);
             console.log(data);
@@ -24,6 +69,22 @@ io.on("connection", (socket) => {
         console.log("Data");
         console.log(data);
         console.log("Data parsed");
+    });
+    //check if player is ready
+    socket.on("Ready", data => {
+        console.log("Ready");
+        console.log(data);
+        socket.broadcast.emit("Ready", data);
+    });
+    //called when player attacks a tile
+    socket.on("Attack", data => {
+        console.log("Attack");
+        console.log(data);
+        socket.broadcast.emit("Attack", data);
+    });
+    //when player disconnects
+    socket.on("disconnect", () => {
+        removePlayer(socket);
     });
     //console.log("a user connected");
 });
